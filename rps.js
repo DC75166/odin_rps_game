d270f3408133fbf9a79f5f578b9e98251454c537
DC75166 <daipayanchakraborty75@gmail.com>
2024-09-28 21:00:32 +0530

    Created play function for the game logic
    Created play function and fetched the
    values from computer and human function. Added
    lowercase method to both the function outputs to
    make them case insensitive.

--- rps.js
+++ rps.js
@@ -1,20 +1,79 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let numb =  Math.floor((Math.random()*100))+1;
-    console.log(numb);
+    // console.log(numb);
     if(numb<=40){
-        console.log("rock");
+        // console.log("rock");
+        return "rock";
     }
     else if(numb<=70){
-        console.log("paper");
+        // console.log("paper");
+        return "paper";
     }
     else{
-        console.log("scissors");
+        // console.log("scissors");
+        return "scissors";
     }
 }
+
 function getHumanChoice(){
    let choice = prompt("Enter your choice");
-   console.log(choice);
+        return choice;
 }
 
-getComputerChoice();
-getHumanChoice();
+function playRound(humanChoice,computerChoice){
+
+   humanChoice = humanChoice.toLowerCase();
+
+    let loss = "you loose";
+    let win = "you win";
+
+    if(humanChoice == "rock"){
+        if(computerChoice == "paper"){
+            console.log(`${loss} . ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+        else if(computerChoice == "scissors"){
+            console.log(`${win} . ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+    }
+
+    if(humanChoice == "paper"){
+        if(computerChoice == "rock"){
+            console.log(`${win}.${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        else if (computerChoice == "scissors"){
+            console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+    }
+
+    if (humanChoice == "scissors"){
+        if(computerChoice == "paper"){
+            console.log(`${win}.${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        else if(computerChoice == "rock"){
+            console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+    }
+
+    if(humanChoice == computerChoice){
+        console.log("Its a tie!");
+        humanScore++;
+        computerScore++;
+    }
+    console.log(`Your Score : ${humanScore}`);
+    console.log(`Computer's Score : ${computerScore}`);
+}
+
+const humanselection = getHumanChoice().toLowerCase();
+const computerselection = getComputerChoice().toLowerCase();
+
+playRound(humanselection,computerselection);
+
