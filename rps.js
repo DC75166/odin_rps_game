46fd26892a6b35110e926ac9bb3fecb04d09ab04
DC75166 <daipayanchakraborty75@gmail.com>
2024-09-29 13:04:44 +0530

    Added a confition to filter invalid input
    Added lowercase filter to the inputs to make
    them case insensitive. Added an else to the if
    else condition chain to filter anything other
    than rock, paper, scissors.

--- rps.js
+++ rps.js
@@ -2,78 +2,78 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let numb =  Math.floor((Math.random()*100))+1;
-    // console.log(numb);
-    if(numb<=40){
-        // console.log("rock");
-        return "rock";
+    let numb = Math.floor((Math.random() * 100)) + 1;
+    let str = "";
+    if (numb <= 40) {
+        str = "rock";
     }
-    else if(numb<=70){
-        // console.log("paper");
-        return "paper";
+    else if (numb <= 70) {
+        str = "paper";
     }
-    else{
-        // console.log("scissors");
-        return "scissors";
+    else {
+        str = "scissors";
     }
+    return str.toLowerCase();
 }
 
-function getHumanChoice(){
-   let choice = prompt("Enter your choice");
-        return choice;
+function getHumanChoice() {
+    let choice = prompt("Enter your choice");
+    return choice.toLowerCase();
 }
 
-function playRound(humanChoice,computerChoice){
-
-   humanChoice = humanChoice.toLowerCase();
+function playRound(humanChoice, computerChoice) {
 
     let loss = "you loose";
     let win = "you win";
+    
+    console.log(`Human choice is ${humanChoice}`);
+    console.log(`Computer choice is ${computerChoice}`);
 
-    if(humanChoice == "rock"){
-        if(computerChoice == "paper"){
-            console.log(`${loss} . ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
+    if (humanChoice == "rock") {
+            if (computerChoice == "paper") {
+                console.log(`${loss} . ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            }
+            else if (computerChoice == "scissors") {
+                console.log(`${win} . ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
         }
-        else if(computerChoice == "scissors"){
-            console.log(`${win} . ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
+
+        else if (humanChoice == "paper") {
+            if (computerChoice == "rock") {
+                console.log(`${win}.${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
+            else if (computerChoice == "scissors") {
+                console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            }
         }
+
+        else if (humanChoice == "scissors") {
+            if (computerChoice == "paper") {
+                console.log(`${win}.${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
+            else if (computerChoice == "rock") {
+                console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            }
+        }
+        else {
+            console.log("invalid input");
+        }
+
+        if (humanChoice == computerChoice) {
+            console.log("Its a tie!");
+        }
+
+        console.log(`Your Score : ${humanScore}`);
+        console.log(`Computer's Score : ${computerScore}`);
     }
+const humanselection = getHumanChoice();
+const computerselection = getComputerChoice();
 
-    if(humanChoice == "paper"){
-        if(computerChoice == "rock"){
-            console.log(`${win}.${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-        else if (computerChoice == "scissors"){
-            console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-    }
-
-    if (humanChoice == "scissors"){
-        if(computerChoice == "paper"){
-            console.log(`${win}.${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-        else if(computerChoice == "rock"){
-            console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-    }
-
-    if(humanChoice == computerChoice){
-        console.log("Its a tie!");
-        humanScore++;
-        computerScore++;
-    }
-    console.log(`Your Score : ${humanScore}`);
-    console.log(`Computer's Score : ${computerScore}`);
-}
-
-const humanselection = getHumanChoice().toLowerCase();
-const computerselection = getComputerChoice().toLowerCase();
-
-playRound(humanselection,computerselection);
+playRound(humanselection, computerselection);
 
