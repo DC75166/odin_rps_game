671367536477c1d4c873238da3b1d33bacf314dd
DC75166 <daipayanchakraborty75@gmail.com>
2024-12-29 07:53:04 +0530

    Code to output rock,paper,scissors randomly

--- rps.js
+++ rps.js
@@ -1,79 +1,18 @@
-let humanScore = 0;
-let computerScore = 0;
+function getComputerChoice(){
 
-function getComputerChoice() {
-    let numb = Math.floor((Math.random() * 100)) + 1;
-    let str = "";
-    if (numb <= 40) {
-        str = "rock";
-    }
-    else if (numb <= 70) {
-        str = "paper";
-    }
-    else {
-        str = "scissors";
-    }
-    return str.toLowerCase();
+  var random = Math.floor(Math.random()*100);
+  console.log(random);
+
+  if(random>=0 && random<33){
+    console.log("rock");
+  }
+  else if(random>=33 && random<66){
+    console.log("paper");
+  }
+  else{
+    console.log("scissors");
+  }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice");
-    return choice.toLowerCase();
-}
-
-function playRound(humanChoice, computerChoice) {
-
-    let loss = "you loose";
-    let win = "you win";
-    
-    console.log(`Human choice is ${humanChoice}`);
-    console.log(`Computer choice is ${computerChoice}`);
-
-    if (humanChoice == "rock") {
-            if (computerChoice == "paper") {
-                console.log(`${loss} . ${computerChoice} beats ${humanChoice}`);
-                computerScore++;
-            }
-            else if (computerChoice == "scissors") {
-                console.log(`${win} . ${humanChoice} beats ${computerChoice}`);
-                humanScore++;
-            }
-        }
-
-        else if (humanChoice == "paper") {
-            if (computerChoice == "rock") {
-                console.log(`${win}.${humanChoice} beats ${computerChoice}`);
-                humanScore++;
-            }
-            else if (computerChoice == "scissors") {
-                console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
-                computerScore++;
-            }
-        }
-
-        else if (humanChoice == "scissors") {
-            if (computerChoice == "paper") {
-                console.log(`${win}.${humanChoice} beats ${computerChoice}`);
-                humanScore++;
-            }
-            else if (computerChoice == "rock") {
-                console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
-                computerScore++;
-            }
-        }
-        else {
-            console.log("invalid input");
-        }
-
-        if (humanChoice == computerChoice) {
-            console.log("Its a tie!");
-        }
-
-        console.log(`Your Score : ${humanScore}`);
-        console.log(`Computer's Score : ${computerScore}`);
-    }
-const humanselection = getHumanChoice();
-const computerselection = getComputerChoice();
-
-playRound(humanselection, computerselection);
+getComputerChoice();
 
