a0cee740e0c9c9bcf8350ef619ff297036a4e77f
DC75166 <daipayanchakraborty75@gmail.com>
2025-01-15 19:10:12 +0530

    Created loop for human choice, game not working
    Created for each loop for human choice and added
    eventlistener for click event.Game not working
    as of now

--- rps.js
+++ rps.js
@@ -1,103 +1,97 @@
+
 //function to randomly generate the computer choices
-function getComputerChoice(){
+function getComputerChoice() {
 
-  let random = (Math.floor(Math.random()*100));
-
-  if(random>=0 && random<33){
-    return "rock";
-  }
-  else if(random>=33 && random<66){
-    return "paper";
-  }
-  else{
-    return "scissors";
-  }
+  let index = (Math.floor(Math.random() * 3));
+  console.log(index);
+  let option = ['rock', 'paper', 'scissors'];
+  return option[index];
 }
 
-// Function to get the human input
-function getHumanChoice(){
-   let choice = prompt("Enter your choice");
-   let str = choice.toLowerCase();
-   return str;
-}
+function playGame() {
 
-// Function to play the game for 5 rounds
-function playGame(){
+  // Variables to track the score of both computer and human
+  let humanScore = 0;
+  let computerScore = 0;
 
-// Variables to track the score of both computer and human
-let humanScore = 0;
-let computerScore = 0;
+  for (let i = 0; i <= 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
-// Play 5 rounds
-for(let i=0;i<5;i++){
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  playRound(humanSelection,computerSelection);
-}
+    playRound(humanSelection, computerSelection);
+  }
 
-// Function to play the game for 1 round
-  function playRound(humanChoice,computerChoice){
-    
+  function getHumanChoice() {
+    const buttons = document.querySelector("button");
+
+    buttons.foreach((button) => {
+      button.addEventListener('click', (event) => {
+        let buttonId = event.target;
+        return buttonId.id;
+      })
+    })
+  }
+
+  // Function to play the game for 1 round
+  function playRound(humanChoice, computerChoice) {
+
+    const result = document.querySelector('#results');
+    const hScore = document.querySelector("#hScore");
+    const cScore = document.querySelector("#cScore");
+
     let loss = "you loose";
     let win = "you win";
 
-     if(humanChoice == computerChoice){
-      console.log("Draw");
+    if (humanChoice == computerChoice) {
+      result.textContent = ("Draw");
       computerScore;
       humanScore;
-     }
+    }
 
-     if(humanChoice == "rock"){
-      if(computerChoice == "paper"){
-          console.log(`${loss} . ${computerChoice} beats ${humanChoice}`);
-          computerScore++;
+    if (humanChoice == "rock") {
+      if (computerChoice == "paper") {
+        result.textContent = (`${loss} . ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
       }
-      else if(computerChoice == "scissors"){
-          console.log(`${win} . ${humanChoice} beats ${computerChoice}`);
-          humanScore++;
+      else if (computerChoice == "scissors") {
+        result.textContent = (`${win} . ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
       }
-  }
+    }
 
-  if(humanChoice == "paper"){
-      if(computerChoice == "rock"){
-          console.log(`${win}.${humanChoice} beats ${computerChoice}`);
-          humanScore++;
+    if (humanChoice == "paper") {
+      if (computerChoice == "rock") {
+        result.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
+        humanScore++;
       }
-      else if (computerChoice == "scissors"){
-          console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
-          computerScore++;
+      else if (computerChoice == "scissors") {
+        result.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
+        computerScore++;
       }
-  }
+    }
 
-  if (humanChoice == "scissors"){
-      if(computerChoice == "paper"){
-          console.log(`${win}.${humanChoice} beats ${computerChoice}`);
-          humanScore++;
+    if (humanChoice == "scissors") {
+      if (computerChoice == "paper") {
+        result.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
+        humanScore++;
       }
-      else if(computerChoice == "rock"){
-          console.log(`${loss}.${computerChoice} beats ${humanChoice}`);
-          computerScore++;
+      else if (computerChoice == "rock") {
+        result.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
+        computerScore++;
       }
-  }
+    }
 
-  //  Prints the score for each round
-  console.log(`Your Score : ${humanScore}`);
-  console.log(`Computer's Score : ${computerScore}`);
-}
-
-// Prints the final score
-  if(`${humanScore}`>`${computerScore}`){
-    console.log("Congrats!You won the game");
+    //  Prints the score for each round
+    hScore.textContent = (`Your Score : ${humanScore}`);
+    cScore.textContent = (`Computer's Score : ${computerScore}`);
+    result.appendChild(hScore);
+    result.appendChild(cScore);
   }
-  else if(`${computerScore}`>`${humanScore}`) {
-    console.log("uh oh! Better luck next time");
-  }
-  else{
-    console.log("You guys are tied! Try Again to beat the computer");
-  }
+  // hScore.textContent = (`Your Score : ${humanScore}`);
+  // cScore.textContent = (`Computer's Score : ${computerScore}`);
+  // result.appendChild(hScore);
+  // result.appendChild(cScore);
+  console.log("loop ends");
 }
 
 playGame();
-
-
-
