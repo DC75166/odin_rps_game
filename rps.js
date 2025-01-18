23b7799d1875430ea1cf102e188bc8bfc381e459
DC75166 <daipayanchakraborty75@gmail.com>
2025-01-18 11:26:14 +0530

    logic for playing game untill a player gets 5 pts
    logic implemented for the player or computer
    to reach 5 points before game resets.

--- rps.js
+++ rps.js
@@ -1,39 +1,39 @@
 
 function playGame() {
 
-//function to randomly generate the computer choices
-function getComputerChoice() {
+  //function to randomly generate the computer choices
+  function getComputerChoice() {
 
-let random = (Math.floor(Math.random()*100)+1);
-console.log(random);
-let choice = '';
+    let random = (Math.floor(Math.random() * 100) + 1);
+    console.log(random);
+    let choice = '';
 
-  if(random>=0 && random<33){
-    choice ="rock";
+    if (random >= 0 && random < 33) {
+      choice = "rock";
+    }
+    else if (random >= 33 && random < 66) {
+      choice = "paper";
+    }
+    else {
+      choice = "scissors";
+    }
+    console.log(choice);
+    return choice;
   }
-  else if(random>=33 && random<66){
-    choice =  "paper";
-  }
-  else{
-    choice =  "scissors";
-  }
-  console.log(choice);
-  return choice;
-}
 
-    let humanChoice = '';
-    const buttons = document.querySelectorAll("button");
-
-    buttons.forEach(button => {
-      button.addEventListener('click', () => {
-        humanChoice = button.id;
-        playRound(getComputerChoice(),humanChoice);
+  let humanChoice = '';
+  const buttons = document.querySelectorAll("button");
+  
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      humanChoice = button.id;
+      playRound(getComputerChoice(), humanChoice);
     });
   })
 
-    let humanScore=0;
-    let computerScore=0;
-    
+  let humanScore = 0;
+  let computerScore = 0;
+
   // Function to play the game for 1 round
   function playRound(humanChoice, computerChoice) {
 
@@ -44,50 +44,64 @@ let choice = '';
     let loss = "you loose";
     let win = "you win";
 
-    if (humanChoice == computerChoice) {
-      result.textContent = ("Draw");
-      computerScore;
-      humanScore;
-    }
-
-    if (humanChoice == "rock") {
-      if (computerChoice == "paper") {
-        result.textContent = (`${loss} . ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+    //  Prints final score
+    if ((humanScore) == 5 || (computerScore) == 5) {
+      if (humanScore == computerScore) {
+        result.textContent = "Its a Tie";
       }
-      else if (computerChoice == "scissors") {
-        result.textContent = (`${win} . ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
+      else if (humanScore > computerScore) {
+        result.textContent = "You win!! Congrats";
+      }
+      else {
+        result.textContent = "You Lose! Try again";
       }
     }
+    else {
+      if (humanChoice == computerChoice) {
+        result.textContent = ("Draw");
+        computerScore;
+        humanScore;
+      }
 
-    if (humanChoice == "paper") {
-      if (computerChoice == "rock") {
-        result.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
-        humanScore++;
+      if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+          result.textContent = (`${loss} . ${computerChoice} beats ${humanChoice}`);
+          computerScore++;
+        }
+        else if (computerChoice == "scissors") {
+          result.textContent = (`${win} . ${humanChoice} beats ${computerChoice}`);
+          humanScore++;
+        }
       }
-      else if (computerChoice == "scissors") {
-        result.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+
+      if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+          result.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
+          humanScore++;
+        }
+        else if (computerChoice == "scissors") {
+          result.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
+          computerScore++;
+        }
       }
+
+      if (humanChoice == "scissors") {
+        if (computerChoice == "paper") {
+          result.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
+          humanScore++;
+        }
+        else if (computerChoice == "rock") {
+          result.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
+          computerScore++;
+        }
+      }
+
+      //  Prints the score for each round
+      hScore.textContent = (`Your Score : ${humanScore}`);
+      cScore.textContent = (`Computer's Score : ${computerScore}`);
+      result.appendChild(hScore);
+      result.appendChild(cScore);
     }
-
-    if (humanChoice == "scissors") {
-      if (computerChoice == "paper") {
-        result.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
-        humanScore++;
-      }
-      else if (computerChoice == "rock") {
-        result.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
-        computerScore++;
-      }
-    }
-
-    //  Prints the score for each round
-    hScore.textContent = (`Your Score : ${humanScore}`);
-    cScore.textContent = (`Computer's Score : ${computerScore}`);
-    result.appendChild(hScore);
-    result.appendChild(cScore);
   }
 }
 
