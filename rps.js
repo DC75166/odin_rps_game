46129ad886f23dbbe7161885fe7b792837c345f4
DC75166 <daipayanchakraborty75@gmail.com>
2025-01-19 12:07:07 +0530

    Update score function called once not every round
    Instead of calling updatescore function in every
    round now the score is displayed at the final
    round.

--- rps.js
+++ rps.js
@@ -4,56 +4,51 @@ function playRps() {
   let humanScore = 0;
   let computerScore = 0;
   let humanSelection = '';
+  let computerSelection = '';
   let count = 0;
   const result = document.querySelector('#results');
   const hScore = document.querySelector("#hScore");
   const cScore = document.querySelector("#cScore");
-  // const body = document.querySelector("body");
   const text = document.querySelector("#text");
-  let loss = "you loose";
-  let win = "you win";
+  const buttons = document.querySelectorAll("button");
+  let loss = "You loose";
+  let win = "You win";
 
 
   //function to randomly generate the computer choices
   function getComputerChoice() {
 
     let random = (Math.floor(Math.random() * 100) + 1);
-    console.log(random);
-    let choice = '';
 
     if (random >= 0 && random < 33) {
-      choice = "rock";
+      computerSelection = "rock";
     }
     else if (random >= 33 && random < 66) {
-      choice = "paper";
+      computerSelection = "paper";
     }
     else {
-      choice = "scissors";
+      computerSelection = "scissors";
     }
-    console.log(choice);
-    return choice;
+    return computerSelection;
   }
 
+  // Function to play the game 5 times
   function playGame() {
-    const buttons = document.querySelectorAll("button");
     buttons.forEach(button => {
       button.addEventListener('click', () => {
         if (count == 4) {
           humanSelection = button.id;
-          console.log(humanSelection);
           playRound(humanSelection, getComputerChoice());
           if (humanScore > computerScore) {
             text.textContent = ("You win! Congrats");
-            updateScore();
           }
           else if (computerScore > humanScore) {
             text.textContent = ("Computer Wins! Try again");
-            updateScore();
           }
           else {
             text.textContent = ("Its a tie!");
-            updateScore();
           }
+          updateScore();
         }
         else if (count < 4) {
           humanSelection = button.id;
@@ -72,47 +67,46 @@ function playRps() {
   function playRound(humanChoice, computerChoice) {
 
     if (humanChoice == computerChoice) {
-      text.textContent = ("Draw");
+      text.textContent = ( `Draw, Computer Choice : ${computerChoice} Human Choice : ${humanChoice}`);
       computerScore++;
       humanScore++;
     }
 
     if (humanChoice == "rock") {
       if (computerChoice == "paper") {
-        text.textContent = (`${loss} . ${computerChoice} beats ${humanChoice}`);
+        text.textContent = (`${loss} , ${computerChoice} beats ${humanChoice}`);
         computerScore++;
       }
       else if (computerChoice == "scissors") {
-        text.textContent = (`${win} . ${humanChoice} beats ${computerChoice}`);
+        text.textContent = (`${win} , ${humanChoice} beats ${computerChoice}`);
         humanScore++;
       }
     }
 
     if (humanChoice == "paper") {
       if (computerChoice == "rock") {
-        text.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
+        text.textContent = (`${win} , ${humanChoice} beats ${computerChoice}`);
         humanScore++;
       }
       else if (computerChoice == "scissors") {
-        text.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
+        text.textContent = (`${loss} , ${computerChoice} beats ${humanChoice}`);
         computerScore++;
       }
     }
 
     if (humanChoice == "scissors") {
       if (computerChoice == "paper") {
-        text.textContent = (`${win}.${humanChoice} beats ${computerChoice}`);
+        text.textContent = (`${win} , ${humanChoice} beats ${computerChoice}`);
         humanScore++;
       }
       else if (computerChoice == "rock") {
-        text.textContent = (`${loss}.${computerChoice} beats ${humanChoice}`);
+        text.textContent = (`${loss} , ${computerChoice} beats ${humanChoice}`);
         computerScore++;
       }
     }
-    updateScore();
   }
 
-  //Prints the score for each round
+  //Function to Print the score for each round
   function updateScore() {
     hScore.textContent = (`Your Score : ${humanScore}`);
     cScore.textContent = (`Computer's Score : ${computerScore}`);
@@ -120,9 +114,10 @@ function playRps() {
     text.appendChild(cScore);
   }
 
+  //Function to reset the game after 5 rounds  
   function reset() {
     result.removeChild(text);
-    text.textContent = ("End of Game!Reload to play again");
+    text.textContent = ("End of Game! Reload to play again");
     result.appendChild(text);
   }
 
